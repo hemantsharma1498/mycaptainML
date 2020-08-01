418227a449d38d8ec5b5048aec800f85c45143be
Hemant <60261899+hemantsharma1498@users.noreply.github.com>
2020-08-02 01:46:59 +0530

    Add files via upload

---
+++ mc_ML_1.py
@@ -0,0 +1,13 @@
+list1 = [12, -7, 5, 64, -14]
+
+for num in list1:
+	if num < 0:	continue
+	else:
+		print(num)
+
+list2 = [12, 14, -95, 3]
+
+for another_num in list2:
+	if another_num < 0:	continue
+	else:
+		print(another_num)
